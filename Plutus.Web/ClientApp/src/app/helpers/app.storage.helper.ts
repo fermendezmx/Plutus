285b98f88b1ab850bdf78edca9ce0de11e4174ae
Fernando Méndez <mendezc.jf@gmail.com>
2018-09-06 13:42:53 -0500

    Create StorageHelper

---
+++ Plutus.Web/ClientApp/src/app/helpers/app.storage.helper.ts
@@ -0,0 +1,25 @@
+﻿export class StorageHelper {
+    //#region Public Methods
+
+    public static set(key: string, value: any): void {
+        if (typeof (value) == 'object') {
+            value = JSON.stringify(value);
+        }
+
+        localStorage.setItem(key, value);
+    }
+
+    public static get(key: string): any {
+        let result = localStorage.getItem(key);
+
+        try {
+            result = JSON.parse(result || '');
+        } catch (ex) {
+            // NOP
+        }
+
+        return result;
+    }
+
+    //#endregion
+}
--- Plutus.Web/ClientApp/src/app/helpers/index.ts
+++ Plutus.Web/ClientApp/src/app/helpers/index.ts
@@ -1,5 +1,6 @@
 ﻿//#region Helpers
 
 export { XHRHelper } from './app.xhr.helper';
+export { StorageHelper } from './app.storage.helper';
 
 //#endregion
